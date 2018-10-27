9c71e6be1b3d38d7deb5b560ba9020de6c7c5302
Hamid Khosravian <hamid.kh.1991@gmail.com>
2018-10-27 23:22:15 +0330

    generate controller and add methods base

---
+++ app/controllers/jauth/authentication_controller.rb
@@ -0,0 +1,30 @@
+require_dependency "jauth/application_controller"
+
+module Jauth
+  class AuthenticationController < ApplicationController
+
+    def sign_in
+    end
+
+    def sign_up
+    end
+
+    def sign_out
+    end
+
+    def confirmation
+    end
+
+    def resend_confirmation
+    end
+
+    def forgot_password
+    end
+
+    def reset_password
+    end
+
+    def change_password
+    end
+  end
+end
---
+++ spec/controllers/jauth/authentication_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Jauth
+  RSpec.describe AuthenticationController, type: :controller do
+
+  end
+end
