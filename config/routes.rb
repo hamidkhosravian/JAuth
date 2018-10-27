84c8dce7c4f1fb556998ce931a58fb7829d7cbfb
Hamid Khosravian <hamid.kh.1991@gmail.com>
2018-10-27 23:32:04 +0330

    add routes of authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,14 @@
 Jauth::Engine.routes.draw do
+  resource :authentications, only: [], path: '' do
+    collection do
+      post :sign_up
+      post :sign_in
+      delete :sign_out
+      post :confirmation
+      put :resend_confirmation
+      post :forgot_password
+      put :reset_password
+      put :change_password
+    end
+  end
 end
